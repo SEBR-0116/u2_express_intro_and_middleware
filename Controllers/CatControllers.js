ab2a5a26eeacfcccaf0554ef5f5841860e58012a
CV2Tre <cvlookin@me.com>
2024-02-08 16:27:05 -0500

    completed

---
+++ Controllers/CatControllers.js
@@ -0,0 +1,24 @@
+const cats = [
+    { name: "Salem",
+     color: "black", 
+     lovesLasagna: false },
+    { name: "Garfield", 
+    color: "orange", 
+    lovesLasagna: true },
+    { name: "Heathcliff", 
+    color: "orange", 
+    lovesLasagna: false },
+  ];
+  
+  const getCats = (req, res) => {
+    res.send(cats)
+  }
+  
+
+  const getCatById = (req, res) => {
+    res.send(cats[req.params.id])}
+
+  module.exports = {
+    getCats, 
+    getCatById
+  }
